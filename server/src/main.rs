364575da30c9ec4490e040bd1772b0ff1bc88045
Wally Yang <wallyyang28@gmail.com>
2019-10-14 08:12:01 -0400

    feat: added back server echo

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,8 +5,9 @@ use std::fs::File;
 use std::path::Path;
 
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+// use std::thread;
 
+use std::io;
 use std::io::{Read, Write};
 
 extern crate serde;
@@ -50,17 +51,17 @@ impl Session {
     }
 }
 
-// fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-//     println!("Incoming connection from: {}", stream.peer_addr()?);
-//     let mut buf = [0; 512];
-//     loop {
-//         let bytes_read = stream.read(&mut buf)?;
-//         if bytes_read == 0 {
-//             return Ok(());
-//         }
-//         stream.write(&buf[..bytes_read])?;
-//     }
-// }
+fn handle_client(mut stream: TcpStream) -> Result<(), io::Error> {
+    println!("Incoming connection from: {}", stream.peer_addr()?);
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+        stream.write(&buf[..bytes_read])?;
+    }
+}
 
 fn main() {
     let session = Session::create();
@@ -68,15 +69,12 @@ fn main() {
 
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
 
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Err(e) => eprintln!("failed: {}", e),
-    //         Ok(stream) => {
-    //             thread::spawn(move || {
-    //                 handle_client(stream)
-    //                     .unwrap_or_else(|error| eprintln!("{:?}", error));
-    //             });
-    //         }
-    //     }
-    // }
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => eprintln!("failed: {}", e),
+            Ok(stream) => {
+                handle_client(stream).unwrap_or_else(|e| eprintln!("{:?}", e));
+            }
+        }
+    }
 }
