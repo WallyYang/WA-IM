e6d4734912ca280a3462a0e94ed052d67490ddc9
Wally Yang <wallyyang28@gmail.com>
2019-10-15 19:46:58 -0400

    feat: added new thread for message listening

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,9 @@
-use std::io::{self, BufRead, BufReader, BufWriter, Write};
+use std::io::{self, BufRead, BufReader, Write};
 use std::net::SocketAddr;
 use std::net::TcpStream;
 use std::str;
+use std::sync::mpsc;
+use std::thread;
 
 use client::Server;
 use waim::*;
@@ -25,8 +27,7 @@ fn prompt_for_user() -> User {
 
 /// send a user to server for registration, return true if no duplicate username
 fn register(stream: &mut TcpStream, user: User) -> bool {
-    let mut writer = BufWriter::new(stream.try_clone().unwrap());
-    send_req(&mut writer, ReqType::Register, &user, &String::new());
+    send_req(stream, ReqType::Register, &user, &String::new());
 
     let mut buffer: Vec<u8> = Vec::new();
     let mut reader = BufReader::new(stream);
@@ -41,8 +42,7 @@ fn register(stream: &mut TcpStream, user: User) -> bool {
 /// send a user to server for validation,
 /// return true if username and password matches
 fn validate(stream: &mut TcpStream, user: User) -> bool {
-    let mut writer = BufWriter::new(stream.try_clone().unwrap());
-    send_req(&mut writer, ReqType::Validate, &user, &String::new());
+    send_req(stream, ReqType::Validate, &user, &String::new());
 
     let mut buffer: Vec<u8> = Vec::new();
     let mut reader = BufReader::new(stream);
@@ -87,14 +87,42 @@ fn main() {
         }
     }
 
-    let mut writer = BufWriter::new(stream);
     loop {
+        let c_stream = stream.try_clone().unwrap();
+        thread::spawn(move || loop {
+            if let Some(request) = recv_req(&c_stream) {
+                match request.req_type {
+                    ReqType::Message => println!(
+                        "{}: {}",
+                        request.user.username, request.message
+                    ),
+                    ReqType::List => {
+                        let users: Vec<String> =
+                            serde_json::from_str(&request.message).unwrap();
+                        print!("Online users: ");
+                        for user in users {
+                            print!("{},", user);
+                        }
+                        io::stdout().flush().unwrap();
+                    }
+                    _ => (),
+                }
+            }
+        });
+
         print!("> ");
         io::stdout().flush().unwrap();
 
         let mut msg = String::new();
         io::stdin().read_line(&mut msg).unwrap();
+        let msg = msg.trim();
 
-        send_req(&mut writer, ReqType::Message, &user, msg.trim());
+        if msg.len() > 0 {
+            if msg == ":l" {
+                send_req(&mut stream, ReqType::List, &user, &String::new());
+            } else {
+                send_req(&mut stream, ReqType::Message, &user, msg);
+            }
+        }
     }
 }
